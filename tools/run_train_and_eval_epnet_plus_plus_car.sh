1eaa7a27658c1e83542b116a104ba53b02bf0046
abhikatoldtrafford <abhikatoldtrafford@gmail.com>
2024-03-20 11:17:15 +0530

    latest code

--- tools/run_train_and_eval_epnet_plus_plus_car.sh
+++ tools/run_train_and_eval_epnet_plus_plus_car.sh
@@ -3,10 +3,9 @@
 
 ################################## train CAR
 CUDA_VISIBLE_DEVICES=0 python train_rcnn.py --cfg_file cfgs/CAR_EPNet_plus_plus.yaml \
---batch_size 4 --train_mode rcnn_online --epochs 15 --ckpt_save_interval 1 \
+--batch_size 4 --train_mode rcnn_online --epochs 25 --ckpt_save_interval 1 \
 --output_dir ./log/CAR_EPNet_plus_plus/   \
 --data_path ../data/ \
---train_with_eval \
 --set LI_FUSION.ENABLED True LI_FUSION.ADD_Image_Attention True  CROSS_FUSION True USE_P2I_GATE True \
 DEEP_RCNN_FUSION False USE_IMAGE_LOSS True IMAGE_WEIGHT 1.0 USE_IMAGE_SCORE True USE_IMG_DENSE_LOSS True USE_MC_LOSS True  \
 MC_LOSS_WEIGHT 1.0   I2P_Weight 0.5 P2I_Weight 0.5  ADD_MC_MASK True MC_MASK_THRES 0.2 SAVE_MODEL_PREP 0.8
