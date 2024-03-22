ff6a68d3a5e71168ee5848529b792aac93f46ce8
abhikatoldtrafford <abhikatoldtrafford@gmail.com>
2024-03-22 12:10:49 +0530

    latest code

--- tools/run_all_eval_epnet_plus_plus_models.sh
+++ tools/run_all_eval_epnet_plus_plus_models.sh
@@ -7,11 +7,19 @@
 # --set LI_FUSION.ENABLED True LI_FUSION.ADD_Image_Attention True  CROSS_FUSION True USE_P2I_GATE True \
 # DEEP_RCNN_FUSION False USE_IMAGE_LOSS True IMAGE_WEIGHT 1.0 USE_IMAGE_SCORE True
 
-CUDA_VISIBLE_DEVICES=0 python eval_rcnn.py --cfg_file cfgs/CAR_EPNet_plus_plus.yaml --eval_mode rcnn_online  \
+# CUDA_VISIBLE_DEVICES=0 python eval_rcnn.py --cfg_file cfgs/CAR_EPNet_plus_plus.yaml --eval_mode rcnn_online  \
+# --output_dir ./epnet_plus_plus_released_trained_models/CAR/eval_results/ \
+# --data_path ../data/ --ckpt ./log/CAR_EPNet_plus_plus/ckpt/checkpoint_epoch_25.pth \
+# --set LI_FUSION.ENABLED True LI_FUSION.ADD_Image_Attention True  CROSS_FUSION True USE_P2I_GATE True \
+# DEEP_RCNN_FUSION False USE_IMAGE_LOSS True IMAGE_WEIGHT 1.0 USE_IMAGE_SCORE True
+
+
+CUDA_VISIBLE_DEVICES=0 python eval_rcnn.py --cfg_file cfgs/CAR_EPNet_plus_plus.yaml --eval_mode rcnn_online \
 --output_dir ./epnet_plus_plus_released_trained_models/CAR/eval_results/ \
---data_path ../data/ --ckpt_dir ./log/CAR_EPNet_plus_plus/ckpt \
+--data_path ../data/ --ckpt ./log/CAR_EPNet_plus_plus/ckpt/checkpoint_epoch_25.pth \
 --set LI_FUSION.ENABLED True LI_FUSION.ADD_Image_Attention True  CROSS_FUSION True USE_P2I_GATE True \
-DEEP_RCNN_FUSION True USE_IMAGE_LOSS True IMAGE_WEIGHT 1.0 USE_IMAGE_SCORE True
+DEEP_RCNN_FUSION False USE_IMAGE_LOSS True IMAGE_WEIGHT 1.0 USE_IMAGE_SCORE True USE_IMG_DENSE_LOSS True USE_MC_LOSS True  \
+MC_LOSS_WEIGHT 1.0   I2P_Weight 0.5 P2I_Weight 0.5  ADD_MC_MASK True MC_MASK_THRES 0.2 SAVE_MODEL_PREP 0.8
 
 # CUDA_VISIBLE_DEVICES=0 python eval_rcnn.py --cfg_file cfgs/CAR_EPNet_plus_plus.yaml --eval_mode rcnn_online --output_dir ./log/CAR_EPNet_plus_plus/eval_results/ \
 # --data_path ../data/ \
